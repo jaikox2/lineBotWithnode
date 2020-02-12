07f67d13a598e24bf5f9e823c5dcabe6456290be
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-12 15:26:54 +0700

    fetch in node

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const accessToken = process.env.accessToken || 'NrkNVLQYgwL0kDGEFIjulNQ5hmHGak8hzEvfpcTnP0pXoZDAq0ByR8XBekWdCe+t+T53vr2gR5BdVTrou/xfjDJOTMhOEQHi6XcdwhO6NH+PXgJqKRz/gghqMS1DM+CFSUOIziUP+nVM8l0xGN/k/QdB04t89/1O/w1cDnyilFU=';
 
 async function replyWebhook(replyToken, msg) {
@@ -8,7 +10,6 @@ async function replyWebhook(replyToken, msg) {
       text: msg,
     }],
   });
-  // eslint-disable-next-line no-undef
   fetch('https://api.line.me/v2/bot/message/reply', {
     method: 'POST',
     headers: {
