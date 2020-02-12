d373dde45a94b3fc1bf3da1576e8a78cd9395cda
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-12 17:20:19 +0700

    test get Info 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const serviceIdx = require('../services/index');
 
 router.post('/webhook', (req, res) => {
-  const { replyToken } = req.body.events[0];
-  const msg = req.body.events[0].message.text;
-  serviceIdx.replyWebhook(replyToken, msg);
+  serviceIdx.replyWebhook(req.body);
   res.status(200).send('webhook');
 });
 
--- src/services/index.js
+++ src/services/index.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch');
 const accessToken = process.env.accessToken || 'NrkNVLQYgwL0kDGEFIjulNQ5hmHGak8hzEvfpcTnP0pXoZDAq0ByR8XBekWdCe+t+T53vr2gR5BdVTrou/xfjDJOTMhOEQHi6XcdwhO6NH+PXgJqKRz/gghqMS1DM+CFSUOIziUP+nVM8l0xGN/k/QdB04t89/1O/w1cDnyilFU=';
 
 
-async function getInfo(replyToken) {
+async function getInfo(userid) {
   try {
-    const reponse = await fetch('https://api.line.me/v2/profile', {
+    const reponse = await fetch(`https://api.line.me/v2/bot/profile/{${userid}}`, {
       method: 'GET',
       headers: {
         // eslint-disable-next-line quote-props
-        'Authorization': `Bearer {${replyToken}}`,
+        'Authorization': `Bearer {${accessToken}}`,
       },
     });
     return reponse;
@@ -19,8 +19,10 @@ async function getInfo(replyToken) {
   }
 }
 
-async function replyWebhook(replyToken, msg) {
-  console.log(await getInfo(replyToken));
+async function replyWebhook(data) {
+  const { replyToken } = data.events[0];
+  const msg = data.events[0].message.text;
+  console.log('info ', await getInfo(data.events[0].source.userId));
   const body = JSON.stringify({
     replyToken,
     messages: [{
