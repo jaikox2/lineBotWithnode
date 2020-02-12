3bb09fc55649c56ec01a3216c09dfd1639eb89eb
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-12 14:47:36 +0700

    change get to post in webhook

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const serviceIdx = require('../services/index');
 
-router.get('/webhook', (req, res) => {
+router.post('/webhook', (req, res) => {
   const { replyToken } = req.body.events[0];
   const msg = req.body.events[0].message.text;
   serviceIdx.replyWebhook(replyToken, msg);
