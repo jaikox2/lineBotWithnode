6ea19a8571de93de944411b32390a4e03a866999
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-12 17:02:40 +0700

    test get Info

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,25 @@ const fetch = require('node-fetch');
 
 const accessToken = process.env.accessToken || 'NrkNVLQYgwL0kDGEFIjulNQ5hmHGak8hzEvfpcTnP0pXoZDAq0ByR8XBekWdCe+t+T53vr2gR5BdVTrou/xfjDJOTMhOEQHi6XcdwhO6NH+PXgJqKRz/gghqMS1DM+CFSUOIziUP+nVM8l0xGN/k/QdB04t89/1O/w1cDnyilFU=';
 
+
+async function getInfo(replyToken) {
+  try {
+    const reponse = await fetch('https://api.line.me/v2/profile', {
+      method: 'GET',
+      headers: {
+        // eslint-disable-next-line quote-props
+        'Authorization': `Bearer {${replyToken}}`,
+      },
+    });
+    return reponse;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
 async function replyWebhook(replyToken, msg) {
+  console.log(await getInfo(replyToken));
   const body = JSON.stringify({
     replyToken,
     messages: [{
