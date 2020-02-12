910d7f1c872c6e00253d08a1913f11532dc4667c
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-12 15:08:24 +0700

    prefix /api/

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(routeIdx);
+app.use('/api', routeIdx);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
