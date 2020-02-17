0c7a7a9b52b4ea1162458cdbde10a5b72d674584
Admin <admin@MacBook-Pro-khxng-Admin.local>
2020-02-17 16:37:16 +0700

    fix image resize

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,22 @@ router.post('/webhook', (req, res) => {
   res.status(200).send('webhook');
 });
 
-router.get('/images/menu/1040', (req, res) => {
-  res.download('./src/public/images/linerichmenu.jpg');
+router.get('/images/menu/:size', (req, res) => {
+  const { size } = req.params;
+  switch (size) {
+    case '240':
+      return res.download('./src/public/images/menu/240.jpg');
+    case '300':
+      return res.download('./src/public/images/menu/300.jpg');
+    case '640':
+      return res.download('./src/public/images/menu/640.jpg');
+    case '700':
+      return res.download('./src/public/images/menu/700.jpg');
+    case '1040':
+      return res.download('./src/public/images/menu/1040.jpg');
+    default:
+      return res.download('./src/public/images/menu/300.jpg');
+  }
 });
 
 module.exports = router;
